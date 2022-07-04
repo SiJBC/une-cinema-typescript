ec20373c60da4a11b5cc285ddb16ad30a8a4bc36
Simon Colman <55180479+SiJBC@users.noreply.github.com>
2022-07-04 22:10:13 +1000

    config

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('hello, AWP');
+"use strict";
+var _a;
+var STATUS;
+(function (STATUS) {
+    STATUS["AVAILABLE"] = "AVAILABLE";
+    STATUS["OCCUPIED"] = "OCCUPIED";
+    STATUS["SELECTED"] = "SELECTED";
+})(STATUS || (STATUS = {}));
+class Seat {
+    constructor(id, isOccupied = false) {
+        this.id = id;
+        this.status = isOccupied ? STATUS.OCCUPIED : STATUS.AVAILABLE;
+        this.element = document.createElement("div");
+        this.element.classList.add('seat');
+        this.element.classList.add(this.status.toLowerCase());
+    }
+}
+class Row {
+    constructor(id, seatNumber) {
+        this.id = id;
+        this.seats = Array.from({ length: seatNumber }).map((_, index) => {
+            const seatId = seatNumber * id + index;
+            return new Seat(seatId);
+        });
+        this.element = document.createElement('div');
+        this.element.classList.add('row');
+        this.element.append(...this.seats.map((seat) => seat.element));
+    }
+}
+class SeatMap {
+    constructor(rowNumber, seatNumberPerRow) {
+        this.selectedSeats = [];
+        this.rows = Array.from({ length: rowNumber }).map((_, index) => {
+            return new Row(index, seatNumberPerRow);
+        });
+        this.element = document.createElement('div');
+        this.element.classList.add('seat-map');
+        this.element.append(...this.rows.map((row) => row.element));
+    }
+}
+const seatMap = new SeatMap(7, 9);
+console.log(seatMap)
+const s = document.getElementById('theater')
+
+
+
+
